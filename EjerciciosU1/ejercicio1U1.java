a7906b8f3b5c2ffb8e94ef858f2aabb2d0247f22
Orlandoesol <70718488+Orlandoesol@users.noreply.github.com>
2021-07-01 23:27:36 -0500

    Add files via upload
    Ejercicios Unidad 1

---
+++ EjerciciosU1/ejercicio1U1.java
@@ -0,0 +1,22 @@
+package unidad1;
+import java.util.Scanner;
+
+public class ejercicio1U1 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el primer número:");
+        int num1 = entrada.nextInt();
+        System.out.println("Ingrese el segundo número:");
+        int num2 = entrada.nextInt();
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int division = num1 / num2;
+        int multiplicacion = num1 * num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es igual a " + suma);
+        System.out.println("La resta de " + num1 + " y " + num2 + " es igual a " + resta);
+        System.out.println("La división de " + num1 + " y " + num2 + " es igual a " + division);
+        System.out.println("La multiplicación de " + num1 + " y " + num2 + " es igual a " + multiplicacion);
+        entrada.close();
+
+    }
+}
